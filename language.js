2d600264ed753f311bf247ea0b83b5085aa9343a
yhs99 <khb113322@naver.com>
2024-06-16 07:09:56 +0900

    add component testing data

--- language.js
+++ language.js
@@ -1,9 +1,40 @@
+let url = "https://yhsdev.me/data/component.json";
 
-$.ajax({
-  url : 'https://yhsdev.me/data/component.json',
-  type : "GET",
-  dataType : "json",
-  success: (data) => {
-    console.log(data);
-  }
-});
+console.log(localStorage.getItem("language"));
+if(!localStorage.getItem("language")) {
+  localStorage.setItem("language", "ko");
+  setComponets(localStorage.getItem("language"));
+}else {
+  setComponets(localStorage.getItem("language"));
+}
+
+
+function setComponets(language) {
+  let datas = "";
+  $.ajax({
+    url : url,
+    type : "GET",
+    dataType : "json",
+    success: (data) => {
+      datas = data[language];
+      console.log(datas);
+      console.log(Object.keys(datas))
+      Object.keys(datas).forEach((element) => {
+        console.log(element)
+        $('#'+element).html(datas[element]);
+      });
+    },
+    error : (request, stats, error) => {
+      document.body.innerHTML = `
+      <h1>${stats}, ${error}, ${request}</h1>
+      언어설정 로딩중 오류가 발생했습니다 잠시후 다시 시도해주세요
+      `
+    }
+  });
+}
+
+function changeLang(language) {
+  localStorage.removeItem("language");
+  localStorage.setItem("language", language);
+  location.reload();
+}
